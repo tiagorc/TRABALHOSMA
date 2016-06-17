409a814e04bac661db6277522851de3bc42b67e0
Victor Cotrim <cotrim149@gmail.com>
2016-06-16 23:19:33 -0300

    Adding imports for sensor agent and behaviour

--- src/agents/IRSensorAgent.java
+++ src/agents/IRSensorAgent.java
@@ -1,5 +1,14 @@
 package agents;
 
-public class IRSensorAgent {
+import behaviours.IRSensorBehaviour;
+import jade.core.Agent;
 
+public class IRSensorAgent extends Agent{
+
+	protected void setup(){
+		System.out.println("Setting up Infra Red sensor named: "+ this.getLocalName());
+		
+		this.addBehaviour(new IRSensorBehaviour(this));
+		
+	}
 }
--- src/behaviours/IRSensorBehaviour.java
+++ src/behaviours/IRSensorBehaviour.java
@@ -1,7 +1,26 @@
 package behaviours;
 
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.ProfileImpl;
+import jade.core.Profile;
+import jade.wrapper.PlatformController;
+import jade.wrapper.AgentController;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
 
-public class IRSensorBehaviour {
+public class IRSensorBehaviour extends CyclicBehaviour{
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
